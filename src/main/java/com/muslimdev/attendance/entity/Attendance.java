07228e32de34abb61dabf4554b82967718b3ba2d
herman-xphp <hermanxphp@gmail.com>
2025-08-31 10:24:13 +0800

    feat: add Attendance entity

---
+++ src/main/java/com/muslimdev/attendance/entity/Attendance.java
@@ -0,0 +1,52 @@
+package com.muslimdev.attendance.entity;
+
+import java.time.LocalDateTime;
+
+import com.muslimdev.attendance.enums.AttendanceStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "attendances")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Attendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Employee employee;
+
+    private LocalDateTime checkIn;
+
+    private LocalDateTime checkout;
+
+    private AttendanceStatus status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
