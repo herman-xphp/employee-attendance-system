78294f0516bdcc4068aa126a95f242d954048fe6
herman-xphp <hermanxphp@gmail.com>
2025-09-03 12:53:46 +0800

    feat(employee-repository): add findByEmployeeCode

--- src/main/java/com/muslimdev/attendance/repository/EmployeeRepository.java
+++ src/main/java/com/muslimdev/attendance/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.muslimdev.attendance.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.muslimdev.attendance.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+    Optional<Employee> findByEmployeeCode(String employeeCode);
 }
