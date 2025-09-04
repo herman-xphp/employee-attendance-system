4bf472d7eea15b16631f247f2f774be69a47abbe
herman-xphp <hermanxphp@gmail.com>
2025-09-04 13:16:53 +0800

    feat(overtime): add id field with generated value to Overtime entity

--- src/main/java/com/muslimdev/attendance/entity/Overtime.java
+++ src/main/java/com/muslimdev/attendance/entity/Overtime.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -22,6 +25,8 @@ import lombok.Setter;
 @Builder
 public class Overtime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
