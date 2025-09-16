7a449eba7bdfcc8aa4421c18946e0e7dc57ca9bb
herman-xphp <hermanxphp@gmail.com>
2025-09-16 12:12:08 +0800

    refactor(department): remove unused Builder annotation and ensure proper repository annotation

--- src/main/java/com/muslimdev/attendance/repository/DepartmentRepository.java
+++ src/main/java/com/muslimdev/attendance/repository/DepartmentRepository.java
@@ -1,9 +1,11 @@
 package com.muslimdev.attendance.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.muslimdev.attendance.entity.Department;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
 }
