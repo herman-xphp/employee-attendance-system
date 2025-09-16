28f57f548701be065ca011d210109371ee104bf8
herman-xphp <hermanxphp@gmail.com>
2025-09-16 12:12:58 +0800

    refactor(shift): remove unused Builder annotation

--- src/main/java/com/muslimdev/attendance/entity/Shift.java
+++ src/main/java/com/muslimdev/attendance/entity/Shift.java
@@ -9,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,7 +19,6 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Shift {
 
     @Id
