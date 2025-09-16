21a44f0e9dd103069d296d7998a2749c619f5abb
herman-xphp <hermanxphp@gmail.com>
2025-09-16 12:12:50 +0800

    refactor(overtime): remove unused Builder annotation

--- src/main/java/com/muslimdev/attendance/entity/Overtime.java
+++ src/main/java/com/muslimdev/attendance/entity/Overtime.java
@@ -11,7 +11,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,7 +21,6 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Overtime {
 
     @Id
