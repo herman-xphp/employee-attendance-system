e56ed2bc5a31171d3f0ab8f9ebc6b9f66a5d32ec
herman-xphp <hermanxphp@gmail.com>
2025-09-16 12:05:16 +0800

    feat(api): add message field and update ApiResponse structure

--- src/main/java/com/muslimdev/attendance/dto/ApiResponse.java
+++ src/main/java/com/muslimdev/attendance/dto/ApiResponse.java
@@ -1,12 +1,16 @@
 package com.muslimdev.attendance.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ApiResponse<T> {
-    private String message;
     private T data;
+    private String errors;
 
 }
