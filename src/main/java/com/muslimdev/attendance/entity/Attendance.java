ebdbc7ebf41e0a8dda964dd0f1d4b8ada09101c5
herman-xphp <hermanxphp@gmail.com>
2025-09-17 10:30:49 +0800

    refactor(attendance): remove unused Builder annotation

--- src/main/java/com/muslimdev/attendance/entity/Attendance.java
+++ src/main/java/com/muslimdev/attendance/entity/Attendance.java
@@ -18,7 +18,6 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
